5bf368aff3410cac513410bc47ab2e83128355c8
ano514 <magdamartin06@gmail.com>
2023-11-09 13:47:08 +0100

    ulohy vetvenie2 uprava

--- src/vetvenie/cisla3.java
+++ src/vetvenie/cisla3.java
@@ -6,7 +6,17 @@ public class cisla3 {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         System.out.println("Zadaj 5 cisel");
-        int[] cisla={s.nextInt()};
-        System.out.println(cisla);
+        String cisla=s.next();
+        int sucet=0;
+        System.out.println("Zadané 5 čísla:");
+        for (int i=0;i<=10;i++){
+            if (i%2==0){
+                System.out.println(cisla.substring(i));
+                sucet += Integer.valueOf(cisla.substring(i));
+            }
+            System.out.println("Sucet je: "+sucet);
+            System.out.println("Priemer je: "+(sucet/5));
+
+        }
     }
 }
